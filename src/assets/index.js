b1c079cc6dde1ad4288d2ced58c4370e55f8c176
isabelafsd <isabelafonsecasd@gmail.com>
2023-04-15 18:00:12 -0300

    navbar development

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,8 +6,9 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import logo1 from "./logo1.png";
-import logo2 from "./logo2.png";
+import logoPP from "./logo-PP.png";
+import logoWH from "./logo-WH.png";
+import logoBK from "./logo-BK.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -61,6 +62,7 @@ export {
   carrent,
   jobit,
   tripguide,
-  logo1,
-  logo2,
+  logoPP,
+  logoWH,
+  logoBK,
 };
